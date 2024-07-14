ef280f14099091d75d54f30e5d373d6e34547236
Albin <karlstrom.28@gmail.com>
2024-07-14 16:38:02 +0200

    delete exercise updated

--- main.py
+++ main.py
@@ -22,24 +22,26 @@ class Traininglog:
         for item in self.data:
             last_set = item['sets'][-1][2]
             if date == item["date"] and name == item["name"]:
-                last_set += 1
-                s = exercise['sets']
-
-                s[2] = last_set
-                item['sets'].extend([s])
+                for i in sets:
+                    last_set+=1
+                    i[2] = last_set
+                    item['sets'].extend([i])    
                 break 
         else: 
             self.data.append(exercise)
         self.save_data()
     
-    def sort_dates(self, data):
-        sort_dates = sorted(data, key=lambda date: datetime.strptime(date["date"], "%d/%m/%y"))
-        return sort_dates
+    def sort_dates(self):
+        self.data = sorted(self.data, key=lambda date: datetime.strptime(date["date"], "%d/%m/%y"))
 
-    def show_exercise(self, data):
-        sort_date = self.sort_dates(data)
-        for i in sort_date:
-            print(i)
+    def show_set(self, data):
+        for j in data['sets']:
+            print(f"set: {j[2]}, weight: {j[0]} kgs, reps: {j[1]}")
+
+    def show_exercise(self,data): # här kan vi också ha en jävla massa val. vilken dag osv man vill kika
+        for i in data:
+            for j in i['sets']:
+                print(f"{i['date']}, {i['name']}, set: {j[2]}, weight: {j[0]} kgs, reps: {j[1]}")
 
     def save_data(self):
         if self.data:
@@ -48,27 +50,45 @@ class Traininglog:
                     json.dump(item,f)
                     f.write('\n')
 
-    def count_exercises_today(self): # vrf inte denna fungera ???
-        with open(self.filename, 'r') as f: # läser in allt som en sträng, inte dictionary. därav konvertera? Json sträng?
+    def count_exercises_today(self): 
+        with open(self.filename, 'r') as f: 
             i=1
             for row in f:
-                exercise = json.loads(row) # rstrip behövs ? tar bort spaces. loads() konverterar sträng till dict
+                exercise = json.loads(row) 
                 if (exercise["date"]) == datetime.today().strftime("%d/%m/%y"):
                     i+=1
             return i 
         
-    def remove_exercies(self,num, date): # bör kör with open här, för att öppna alla filerna, och sedan tag fram indexet korrekt
+    def remove_exercies(self,date_idx): 
         with open(self.filename, 'r') as g:
             lines = g.readlines()
 
         new_file=[]
+        date_idx-=1
+        remove_data = self.data[date_idx]
         for line in lines:
             rows = json.loads(line)
-            if (rows["Exercsise count: "] != num or date != rows["date"]): # vad blir skillnad mellan or & and?
-                new_file.append(json.dumps(rows) + '\n') # blev tomt utan json.dumps
+            if rows["date"] != remove_data["date"] or rows["name"] != remove_data["name"]: # and måste båda villkoren vara sanna
+                new_file.append(json.dumps(rows)+'\n')
         
         with open(self.filename, 'w') as f:
             f.writelines(new_file)
+        self.data.pop(date_idx) 
+
+    def remove_set(self, chosen_exercise, remove_idx, date_idx):
+        sets=[]
+        for i in chosen_exercsise['sets']:
+            if i[2] != remove_idx:
+                sets.append(i)
+        chosen_exercise['sets'] = sets
+        self.save_data()
+
+        if not chosen_exercise['sets']:
+            self.remove_exercies(date_idx)
+        # beräkna nu om setnummer
+        for idx,item in enumerate(chosen_exercise['sets'], start=1):
+            item[2] = idx
+        self.save_data()
 
     def load_data(self):
         exercsises=[]
@@ -129,19 +149,16 @@ def main_menu():
     print("Huvudmeny:")
     print("1. Lägg till övning")
     print("2. Visa alla övningar")
-    print("3. Radera en övning")
-    print("4. Plotta graf")
-    #print('ta bort övning')
-    #print('lägg till vikter på övning')
-    #print('redigera övning')
+    print("3. Radera")
+    print("4. Plotta")
+    print('5. Redigera övning')
     print("Tryck quit för att avsluta\n")
 
 def add_sets():
     num_set=0
-    sets=[]
 
     while True:
-        #os.system('cls')
+        os.system('cls')
         print('Lägg till ett set: Y/N')
         answer = input()
         if answer == 'n'.lower():
@@ -156,10 +173,9 @@ def add_sets():
                 reps = int(input())
                 num_set+=1 
                 sets.append([weight, reps, num_set])
-                print(sets)
             except:
                 print("\n *****Sets eller reps bör vara ints***** \n") 
-            #os.system('cls')
+            os.system('cls')
     return sets, num_set
 
 #lägga in så man kan köra back eller quit överallt
@@ -170,13 +186,12 @@ while True:
 
     if command.lower() == "quit":
         break
-    
+    sets=[]
     if command == "1":
         os.system('cls')
         print("1. Lägg till en övning") # fixa så man kan lägga in datum också
         print("2. Välj datum.")
         today = datetime.today().strftime("%d/%m/%y")
-        
         new_command = input()
         if new_command == '1':
             os.system('cls')
@@ -184,39 +199,53 @@ while True:
             sets, num_set = add_sets()
             if sets:
                 Training_log.add_excersise_gym(name, sets, today)
-        if new_command == '2': # date_ == datetime.strptime(date_, "%d/%m/%y") 
+        if new_command == '2':
             os.system('cls')
             print('[Enter: dagens datum] [mm/dd/yy]')
-            date_ = input("Välj datum: ")
+            date = input("Välj datum: ")
             try:
-                if date_ == '':
-                    date_ = today
+                if date == '':
+                    date = today
                     name = input("Övningens namn? ")
                     sets, num_set = add_sets()
-                    Training_log.add_excersise_gym(name, sets, date_)
-                elif date_ == datetime.strptime(date_, "%d/%m/%y").strftime("%d/%m/%y"): # raisar exception, strptime
+                    Training_log.add_excersise_gym(name, sets, date)
+                elif date == datetime.strptime(date, "%d/%m/%y").strftime("%d/%m/%y"): # raisar exception, strptime
                     name = input("Övningens namn? ")
                     sets, num_set = add_sets()
-                    Training_log.add_excersise_gym(name, sets, date_)
+                    Training_log.add_excersise_gym(name, sets, date)
             except:
                 print ( ' **** Fel format på datum. Format: [mm/dd/yy] eller tryck Enter ****  \n')
                     
     if command == '2':
         os.system('cls')
-        Training_log.show_exercise(Training_log.data) # välj datum
+        Training_log.show_exercise(Training_log.data) 
     if command == '3':
         #os.system('cls')
-        print("Välj vilken övning du vill radera [nr] och datum [dd/mm/yy]")
-        Training_log.show_exercise(Training_log.data)
-        num_date = input()
-        num_date = num_date.split()
-        if len(num_date) == 2:
-            number = int(num_date[0])
-            date_ = num_date[1]
-        else:
-            #os.system('cls')
+        unique_names=set()
+        for i in Training_log.data:
+            unique_names.add(i["name"])
+        for i in unique_names:
+            print(i)
+
+        name = input("Välj övning: ") 
+        for idx,item in enumerate(Training_log.data, start=1):
+            if item["name"] == name:
+                print(f"{idx}. {item['date']}") # jag behöver motsvarande idx för hela datasetet
+        try:
+            dates_num = int(input("Välj ett datum: "))
+        except:
             continue
-        Training_log.remove_exercies(number, date_)
+        print("1. Ta bort ett set")
+        print("2. Ta bort hela övningen")
+        new_command = input()
+        if new_command == '1':
+            chosen_exercsise = Training_log.data[dates_num-1]
+            Training_log.show_set(chosen_exercsise)
+            remove_idx = int(input("Välj vilket set du vill ta bort: "))
+            Training_log.remove_set(chosen_exercsise, remove_idx, dates_num)
+
+        if new_command == '2':
+            Training_log.remove_exercies(dates_num) # fixa så den kan tag bort liksom flera övningar samtidigt
         
     if command == '4':
         os.system('cls')
@@ -224,8 +253,7 @@ while True:
             name_exercsise = input("Välj övning: ")
             data_log=[]
             found = False
-            sort_data = Training_log.sort_dates(Training_log.data)
-            for i in sort_data:
+            for i in Training_log.data:
                 if i["name"] == name_exercsise:
                     data_log.append(i)
                     found = True
@@ -242,7 +270,7 @@ while True:
             for idx,item in enumerate(data_log, start=1):
                 indx.append(idx)
                 print(f"{idx}. {item['date']}")
-            dates = input("[nr1 nr2]. Välj mellan vilka datum: ")
+            dates = input("[nr1 nr2]. Välj mellan vilka datum: ") # om längden av dates<2, stop
             dates_idx = dates.split()
 
             if len(dates_idx) == 2:
@@ -272,9 +300,6 @@ while True:
                 print ('Fel format. Försök igen ')
             
     if command == '5':
-        add_sets()
+        pass
 
-# för varje set, göra en lista, som innehåller vikt
-# excersises, 
-# istället för antalet set, så lägger man till övning, med tillhörande reps / sets
 
